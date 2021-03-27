7899fd68cbe307ac789c83b30ed9667bc940670d
Kirua <yo44prg@icloud.com>
2021-03-27 21:19:43 +0100

    feat : switch to V.2.0

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header v-bind:navOpen="false">
+      <Nav/>
+    </Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Nav
+  },
+  // watch:{
+  //   sidebar: function (openNav,element,container) {
+  //     if (openNav == true) {
+  //       element.style.width = "250px";
+  //       container.style.marginRight = "250px";
+  //     }
+  //   }
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+:root {
+  --white: #ffffff;
+  --gray-white: #efefef;
+  --purple: #8d39fa;
+  --black: #000000;
+  --gray: #72726e;
+  --transparent-black: rgba(0, 0, 0, 0.5);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  /* font-family: 'robotoregular'; */
+}
+
+/* #body {
+  transition: margin-right .5s;
+  -webkit-transition: margin-right .5s;
+  -moz-transition: margin-right .5s;
+  -ms-transition: margin-right .5s;
+  -o-transition: margin-right .5s;
+} */
+
+.maxLength {
+  max-width: 1200px;
+  min-width: 1200px;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="sidenav" class="sidenav" v-on:click="test">
+    <!-- <a class="tablink" href="javascript:void(0)" id="closenav" class="closebtn">&times;</a> -->
+    <a class="tablink" href="javascript:void(0)" id="closenav">&times;</a>
+    <a class="tablink" href="#home">Home</a>
+    <a class="tablink" href="#comparator">Comparator</a>
+    <a class="tablink" href="#trend">Trend</a>
+    <a class="tablink" href="#about">About</a>
+    <a class="tablink" href="#contact">Contact</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {
+    navOpen: Boolean
+  },
+  methods: {
+    test : function() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sidenav {
+  height: 100%;
+  width: 20px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  background-color: var(--purple);
+  overflow-x: hidden;
+  padding-top: 60px;
+  transition: 0.5s;
+  -webkit-transition: 0.5s;
+  -moz-transition: 0.5s;
+  -ms-transition: 0.5s;
+  -o-transition: 0.5s;
+  padding-top: 0;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: var(--white);
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+.tablink {
+  background-color: var(--transparent-black);
+  color: white;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  font-size: 17px;
+  width: 100%;
+}
+
+#closenav {
+  padding-left: 80%;
+  font-size: 2.5rem;
+}
+</style>
