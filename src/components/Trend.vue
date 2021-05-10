3cd7f18b0e84c3976e25368bca147aa28f251037
kirua <yo44prg@icloud.com>
2021-05-10 10:04:59 +0200

    feat: modify api routes

--- src/components/Trend.vue
+++ src/components/Trend.vue
@@ -19,41 +19,12 @@
 export default {
   data() {
     return {
-      sneakers: [
-        // {
-        //   imgPath: require("@/assets/sneakers/adidas-ozweego-celox.webp"),
-        //   model: "Adidas Ozweego Celox",
-        //   price: "120,00 €",
-        //   gender: "Mixte",
-        //   sellerName: "courir"
-        // },
-        // {
-        //   imgPath: require("@/assets/sneakers/air-jordan-one.webp"),
-        //   model: "Jordan Air One",
-        //   price: "130,00 €",
-        //   gender: "Homme",
-        //   sellerName: "courir"
-        // },
-        // {
-        //   imgPath: require("@/assets/sneakers/new-balance-327.png"),
-        //   model: "New Balance 327",
-        //   price: "90,00 €",
-        //   gender: "Femme",
-        //   sellerName: "courir"
-        // },
-        // {
-        //   imgPath: require("@/assets/sneakers/puma-mirage-tech.webp"),
-        //   model: "Puma Mirage Tech",
-        //   price: "100,00 €",
-        //   gender: "Homme",
-        //   sellerName: "courir"
-        // }
-      ]
+      sneakers: []
     }
   },
   methods: {
     findData: function() {
-      this.$axios.get("http://localhost:3000/trend").then(response => this.manageData(response));
+      this.$axios.get("https://scrapysneake.herokuapp.com/trend").then(response => this.manageData(response));
     },
     manageData: function(response) {
       console.log(response.data);
