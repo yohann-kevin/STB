ef0d0183f07eb2c51aa3ae70089e42d2dbfc8f7b
Kirua <yo44prg@icloud.com>
2021-05-18 22:03:54 +0200

    fix: refactoring

--- src/main.js
+++ src/main.js
@@ -8,28 +8,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-// test
-// import {findAdmin} from './admin'
-// import router from './router'
+import routes from './routes.js'
 
 // components
 import App from './App.vue'
-import Home from './components/home/Home.vue'
-import Index from './components/admin/Index.vue'
-
-// components admin
-import Stats from './components/admin/page/Stats.vue'
-import Advert from './components/admin/page/Advert.vue'
-import Partners from './components/admin/page/Partners.vue'
-import Scraper from './components/admin/page/Scraper.vue'
-import AddAbout from './components/admin/page/AddAbout.vue'
-import Message from './components/admin/page/Message.vue'
-import Parameter from './components/admin/page/Parameter.vue'
-
-// error components
-import Error404 from './components/error/404.vue'
-
-let isAdmin = false;
 
 Vue.use(VueRouter)
 
@@ -40,66 +22,6 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
-const routes = [
-  { 
-    path: "/", 
-    name: "Home",
-    component: Home 
-  },
-  {
-    path: "/admin/:isAdmin",
-    name: "IndexAdmin",
-    component: Index,
-    beforeEnter(to, from, next) {
-      isAdmin = (to.params.isAdmin === "kirua&Kercode4");
-      if (from.name == "Home" && isAdmin) next({path: "/admin"});
-      else next({path: "/"});
-    }
-  },
-  {
-    path: "/admin",
-    name: "IndexAdmin",
-    component: Index,
-    beforeEnter(to, from, next) {
-      isAdmin ? next() : next({path: "/"});
-    },
-    children: [
-      {
-        path: "/admin",
-        component: Stats
-      },
-      {
-        path: "/advert",
-        component: Advert
-      },
-      {
-        path: "/partners",
-        component: Partners
-      },
-      {
-        path: "/scraper",
-        component: Scraper
-      },
-      {
-        path: "/about",
-        component: AddAbout
-      },
-      {
-        path: "/message",
-        component: Message
-      },
-      {
-        path: "/parameter",
-        component: Parameter
-      }
-    ]
-  },
-  {
-    path: "*",
-    name: "Error404",
-    component: Error404
-  },
-]
 
 const router = new VueRouter({ routes })
 
---
+++ src/routes.js
@@ -0,0 +1,81 @@
+// components
+import Home from './components/home/Home.vue'
+import Index from './components/admin/Index.vue'
+
+// components admin
+import Stats from './components/admin/page/Stats.vue'
+import Advert from './components/admin/page/Advert.vue'
+import Partners from './components/admin/page/Partners.vue'
+import Scraper from './components/admin/page/Scraper.vue'
+import AddAbout from './components/admin/page/AddAbout.vue'
+import Message from './components/admin/page/Message.vue'
+import Parameter from './components/admin/page/Parameter.vue'
+
+// error components
+import Error404 from './components/error/404.vue'
+
+
+let isAdmin = false;
+
+var routes = [
+  { 
+    path: "/", 
+    name: "Home",
+    component: Home 
+  },
+  {
+    path: "/admin/:isAdmin",
+    name: "IndexAdmin",
+    component: Index,
+    beforeEnter(to, from, next) {
+      isAdmin = (to.params.isAdmin === "kirua&Kercode4");
+      if (from.name == "Home" && isAdmin) next({path: "/admin"});
+      else next({path: "/"});
+    }
+  },
+  {
+    path: "/admin",
+    name: "IndexAdmin",
+    component: Index,
+    beforeEnter(to, from, next) {
+      isAdmin ? next() : next({path: "/"});
+    },
+    children: [
+      {
+        path: "/admin",
+        component: Stats
+      },
+      {
+        path: "/advert",
+        component: Advert
+      },
+      {
+        path: "/partners",
+        component: Partners
+      },
+      {
+        path: "/scraper",
+        component: Scraper
+      },
+      {
+        path: "/about",
+        component: AddAbout
+      },
+      {
+        path: "/message",
+        component: Message
+      },
+      {
+        path: "/parameter",
+        component: Parameter
+      }
+    ]
+  },
+  {
+    path: "*",
+    name: "Error404",
+    component: Error404
+  },
+]
+
+export default routes;
