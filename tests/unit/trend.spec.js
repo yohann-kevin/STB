6e496f7ba0e0aaed200f2524854de4e47cd20338
Kirua <yo44prg@icloud.com>
2021-08-22 17:34:33 +0200

    feat: update spec

--- tests/unit/trend.spec.js
+++ tests/unit/trend.spec.js
@@ -1,15 +1,20 @@
-import { mount } from "@vue/test-utils";
+import { mount, shallowMount } from "@vue/test-utils";
 import Trend from "@/components/home/section/Trend.vue";
 import axios from 'axios';
 
 describe("Trend.vue", () => {
-  const wrapper = mount(Trend,  { mocks: { $axios: axios } });
+  const wrapper = shallowMount(Trend,  { mocks: { $axios: axios } });
   
   it("check title of page", () => {
     expect(wrapper.text()).toMatch("Tendance");
   });
 
   it("check trend data is not empty for trend section", () => {
+    // console.log(wrapper.vm._data.num)
+    console.log(wrapper.vm.findData())
+    console.log(wrapper.vm._data.sneakers)
+    // console.log(wrapper.findAll('.last-trend').contains("article"))
+    // console.log(wrapper.findAll('.sneakerTrend').exists())
     expect(wrapper.find(".sneakerTrend")).not.toBeUndefined();
   });
 });
