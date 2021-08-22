ea8b0da6f3034284e158afeb29f8eba5c2879f94
Kirua <yo44prg@icloud.com>
2021-08-22 18:03:43 +0200

    feat: replace spec

---
+++ tests/unit/trend.spec.js
@@ -0,0 +1,15 @@
+import { mount } from "@vue/test-utils";
+import Trend from "@/components/home/section/Trend.vue";
+import axios from 'axios';
+
+describe("Trend.vue", () => {
+  const wrapper = mount(Trend,  { mocks: { $axios: axios } });
+  
+  it("check title of page", () => {
+    expect(wrapper.text()).toMatch("Tendance");
+  });
+
+  it("check trend data is not empty for trend section", () => {
+    expect(wrapper.find(".sneakerTrend")).not.toBeUndefined();
+  });
+});
