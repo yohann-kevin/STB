484cdb65e6d91de9f137e1c0dd659386d20fba07
Kirua <yo44prg@icloud.com>
2021-10-17 18:34:15 +0200

    feat: add components most seller appear

--- src/components/admin/page/Stats.vue
+++ src/components/admin/page/Stats.vue
@@ -3,77 +3,25 @@
     <h1>Stats</h1>
     <div class="data-viz">
       <NumberSneaker :sneakerCount="findCount()"/>
-      <div class="visualisation">
-        <h2 class="visu-title">Plop :</h2>
-        <canvas ref="colorchart" id="color-chart" class="color-chart"></canvas>
-      </div>
+      <MostSellerAppear/>
     </div>
   </div>
 </template>
 
 <script>
 import NumberSneaker from "./visualization/NumberSneaker.vue";
-
-import Chart from "chart.js";
+import MostSellerAppear from "./visualization/MostSellerAppear"
 
 export default {
   components: {
-    NumberSneaker
-  },
-  data() {
-    return {
-      sneakerCount: null,
-      dataBar: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      optionsBar: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    }
+    NumberSneaker,
+    MostSellerAppear
   },
   methods: {
     findCount: function() {
       this.$axios.get(process.env.VUE_APP_API_LINK + "/sneakers/count/all").then(response => this.sneakerCount = response.data)
       return this.sneakerCount;
-    },
-    chartLine: function() {
-      let ctx = this.$refs.colorchart;
-      let data = this.dataBar;
-      let options = this.optionsBar;
-      new Chart(ctx, {
-          type: 'bar',
-          data,
-          options
-      });
     }
-  },
-  mounted() {
-    this.chartLine();
   }
 }
 </script>
---
+++ src/components/admin/page/visualization/MostSellerAppear.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="visualisation">
+    <h2 class="visu-title">Plop :</h2>
+    <canvas ref="colorchart" id="color-chart" class="color-chart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+// import moment from 'moment';
+
+export default {
+  data() {
+    return {
+      sneakerCount: null,
+      dataBar: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      optionsBar: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    chartLine: function() {
+      let ctx = this.$refs.colorchart;
+      let data = this.dataBar;
+      let options = this.optionsBar;
+      new Chart(ctx, {
+          type: 'bar',
+          data,
+          options
+      });
+    }
+  },
+  mounted() {
+    this.chartLine();
+  }
+}
+</script>
+
+<style>
+
+</style>
